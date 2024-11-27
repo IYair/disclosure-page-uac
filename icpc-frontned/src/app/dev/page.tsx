852ae54f794aa0a7530318b878baf84180d181f8
Mario-Landa <m.landa.lopez@hotmail.com>
2024-11-27 14:59:04 -0600

    Memory ya chido

--- icpc-frontned/src/app/dev/page.tsx
+++ icpc-frontned/src/app/dev/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
       <CreateNewsComponent />
       <CreateNoteComponent />
       <CreateExcerciseComponent />
-
       <CreateCategoryComponent />
       <CreateDifficultyComponent />
       <CreateMemoryComponent />
