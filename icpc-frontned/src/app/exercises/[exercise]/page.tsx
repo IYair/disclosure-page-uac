f774129881e8c83a6636919fbc3261bc98661d6b
RodriguezRamirezGerardoOmar <al057564@uacam.mx>
2025-05-21 11:11:26 -0600

    feat(itemLogs): Delay log requests for 30s
    Created a new Heartbeat component that sends the log requests after 30s of being rendered. After that, it loops and sends log requests every 5 minutes

--- icpc-frontned/src/app/exercises/[exercise]/page.tsx
+++ icpc-frontned/src/app/exercises/[exercise]/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import useStore from '@/store/useStore'
 import ExerciseCardComponent from '@/app/components/cards/ExerciseCardComponent'
 import useExcerciseStore from '@/store/useExcerciseStore'
 import { serialize } from 'next-mdx-remote/serialize'
 import rehypeKatex from 'rehype-katex'
 import remarkMath from 'remark-math'
+import HeartbeatComponent from '@/app/components/logging/HeartbeatComponent'
 
 async function getMarkdown(body: string) {
   return await serialize(body, {
@@ -18,14 +18,11 @@ async function getMarkdown(body: string) {
 async function ExercisePage({ params }: Readonly<{ params: { exercise: string } }>) {
   const exerciseBody = await useExcerciseStore.getState().getExercise(params.exercise)
   const description = await getMarkdown(exerciseBody.description)
-  const isLoggedIn = useStore.getState().isLogged
-  const solution = await getMarkdown(exerciseBody.solution)
-  if (!isLoggedIn) {
-    useExcerciseStore.getState().log(params.exercise)
-  }
+  const solution = await getMarkdown(exerciseBody.solution)  
 
   return (
     <main className='grid min-h-screen grid-cols-1 place-items-center justify-between w-full py-24'>
+      <HeartbeatComponent itemId={params.exercise} itemType='exercise' />
       <div className='flex justify-end w-full'>
         <ExerciseCardComponent
           exercise={exerciseBody}
